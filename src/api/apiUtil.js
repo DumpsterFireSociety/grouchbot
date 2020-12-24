b35e07974538d2322480f77597208b322beb5df3
Alex Guyot <ajguyot@icloud.com>
2020-12-24 14:53:12 -0700

    Tweak logging to skip chatty Ambassador calls, don't @channel for startups since this bot isn't urgent

--- src/api/apiUtil.js
+++ src/api/apiUtil.js
@@ -4,9 +4,19 @@ const CBLogger = require('@unplgtc/cblogger');
 
 const apiUtil = {
 	logCall(req, res, next) {
-		CBLogger.info('api_call_received', { url: req.originalUrl });
+		const url = req.originalUrl,
+		      method = req.method;
+
+		if (![ 
+				'/healthcheck',
+				'/readycheck',
+				'/.ambassador-internal/openapi-docs'
+			].includes(url)
+		) {
+			CBLogger.info('api_call_received', { method, url });
+		}
 		next();
-	}
+	},
 }
 
 module.exports = apiUtil;
